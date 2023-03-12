c06c5daffadef70c2e3447046361e461fa55a7c6
T_Ferrarah <ferrara.tommaso@panettipitagora.edu.it>
2023-03-12 18:45:23 +0100

    Implemented Joke display

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 import Title from './components/Title/Title'
 import Subtitle from './components/Subtitle/Subtitle'
 import Paragraph from './components/Paragraph/Paragraph'
+import Joke from './components/Joke/Joke'
+import Button from './components/Button/Button'
 
 function App() {
 
@@ -10,6 +13,14 @@ function App() {
   <Paragraph content='Joke Norris usa le API di chucknorris.io per generare delle battute casuali.' align='right' />
   <Paragraph content='Se te le vuoi scegliere, sei nel posto sbagliato.' align='center' />
 </div>
+
+  const [joke, setJoke] = useState('');
+  // Load a joke on page load using useEffect
+  useEffect(() => {
+    fetch('https://api.chucknorris.io/jokes/random')
+      .then(response => response.json())
+      .then(data => setJoke(data.value))
+  }, [])
 
   return (
     <div className="App">
@@ -20,6 +31,10 @@ function App() {
       {/* Display description only on desktop using js*/}
       {window.innerWidth > 768 && description}
       <img src="./Illustration.png" alt="Chuck Norris API Illustration" />
+      <div className="jokeContainer">
+        <Joke content={joke} />
+        <Button value="Copia 📋" onClick={() => navigator.clipboard.writeText(joke)} />
+      </div>
     </div>
   )
 }
