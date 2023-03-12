80dcb82efab7a3b05928ef2c6102d2d910da1496
TFerrarah-alt <ferrara.tommaso@panettipitagora.edu.it>
2023-03-12 12:47:06 +0100

    Titles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Title from './components/Title/Title'
+import Subtitle from './components/Subtitle/Subtitle'
 
 function App() {
 
   return (
     <div className="App">
-      <Title title="Joke Norris" />
+      <div className="titles">
+        <Title title="Joke Norris" />
+        <Subtitle subtitle = "Chuck Norris Joke Generator" />
+      </div>
     </div>
   )
 }
