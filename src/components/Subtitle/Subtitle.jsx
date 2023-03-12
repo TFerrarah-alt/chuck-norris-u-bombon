0afcc1944ef9297f6f6642692e7f06dd3676a9a8
TFerrarah-alt <ferrara.tommaso@panettipitagora.edu.it>
2023-03-12 12:46:53 +0100

    Created subtitle component w/story

---
+++ src/components/Subtitle/Subtitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Subtitle.css';
+
+function Subtitle({subtitle}) {
+    return (
+        <h2>{subtitle}</h2>
+    );
+}
+
+export default Subtitle;
---
+++ src/components/Subtitle/Subtitle.stories.jsx
@@ -0,0 +1,18 @@
+// Button.stories.js|jsx
+
+import { React, useState } from 'react';
+
+import Subtitle from './Subtitle.jsx'
+
+export default {
+  title: 'Subtitle',
+  component: Subtitle,
+};
+
+const Template = (args) => <Subtitle {...args} />;
+
+export const Normal = Template.bind({});
+
+Normal.args = {
+    subtitle: "Chuck Norris Joke Generator",
+};
