5c258e45b8ba47b866f8abd80e09597c6e81f5df
T_Ferrarah <ferrara.tommaso@panettipitagora.edu.it>
2023-03-12 19:16:45 +0100

    Improved conditional styling

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.css';
 
-function Button({value, onClick, disabled}) {
+function Button({value, onClick, disabled, width}) {
     return (
-        <button className="btn" style={disabled ? {opacity: 0.4} : {}} onClick={onClick}>
+        <button className="btn" style={{width: width, opacity: disabled && 0.4, cursor: disabled ? 'default' : 'pointer'}} onClick={onClick}>
             {value}
         </button>
     );
